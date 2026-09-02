6cad332110b7cd511fa4431caede404e43e3cb0d
Debdut <debdutdeb@outlook.com>
2026-09-03 01:47:16 +0530

    feat: add `ReadHasher` and sha256 hasher implementation

---
+++ stdutils/reader/hasher.go
@@ -0,0 +1,38 @@
+package reader
+
+import (
+	"crypto/sha256"
+	"hash"
+	"io"
+)
+
+// ReadHasher implements both [io.Reader] and [hash.Hash] interfaces.
+//
+// This allows any ReadHasher to provide the underlying hash of the data that's been read.
+type ReadHasher interface {
+	io.Reader
+	hash.Hash
+}
+
+type readHasher struct {
+	io.Reader
+
+	hash.Hash
+}
+
+// Sha256Hasher returns a [ReadHasher] that can be used to get the sha256 hash of 
+// the underlying bytes that's been read.
+//
+// Example:
+//
+//	reader := readerutils.Sha256Hasher(f)
+//	_ = io.ReadAll(reader)
+//	fmt.Println(hex.EncodeToString(reader.Sum(nil)))
+func Sha256Hasher(reader io.Reader) ReadHasher {
+	h := sha256.New()
+	return &readHasher{
+		Reader: io.TeeReader(reader, h),
+		Hash:       h,
+	}
+}
+
