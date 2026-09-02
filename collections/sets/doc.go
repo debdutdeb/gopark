63c68acf027dbe59f08d70f3a9ccd50e3cd6e96f
Debdut <debdutdeb@outlook.com>
2026-09-02 12:22:14 +0530

    doc(collections/sets): reformat for godoc

--- collections/sets/doc.go
+++ collections/sets/doc.go
@@ -1,31 +1,43 @@
 /*
-- `sets` is a convinience package for re-type of a unique element list.
+sets is a convinience package for re-type of a unique element list.
 
-- Under the hood it is a map, therefore beyond the package level functions, all `maps` and `iter` package functions get carried over.
+Under the hood it is a map, therefore beyond the package level functions, all [maps] and [iter] packages' functions get carried over.
 
-- Only basic set functions have typed functions in this package.
+Only basic set functions have typed functions in this package.
 
-- For iterating, simply use a `range` loop
+# Iterating
 
-  - for element := range set {
-  - // use element
+Simply use a range loop:
 
-- }
+	for element := range set {
+		// use element
+	}
 
-- All `maps` and `iter` package helpers get carried over. For example,
-- To convert a set to a slice
+# Composability with stdlib
 
-  - elements := slices.Collect(maps.Keys(set))
+All [maps] and [iter] package helpers get carried over. For example
 
-- Or to get just one element off of a set (a bit of an overkill over using range, but for example's sake)
-  - next, _ := iter.Pull(maps.Keys(set))
-  - e, _ := next()
+# To convert a set to a slice
 
-- Adding more wrapper in sets package therefore does not make sense.
-- Convert any map to a set simply through `sets.Collect`
-  - sets.Collect(maps.Keys(someMap))
-  - sets.Collect(maps.Values(someMap))
+Use [slices] package.
 
-- Clone an existing set using `sets.Clone`
+  elements := slices.Collect(maps.Keys(set))
+
+Or to get just one element off of a set (a bit of an overkill over using range, but for example's sake)
+
+
+  next, _ := iter.Pull(maps.Keys(set))
+  e, _ := next()
+
+
+# Extra
+
+Adding more wrapper in sets package therefore does not make sense.
+Convert any map to a set simply through [sets.Collect]
+
+  sets.Collect(maps.Keys(someMap))
+  sets.Collect(maps.Values(someMap))
+
+Clone an existing set using `sets.Clone`
 */
 package sets
