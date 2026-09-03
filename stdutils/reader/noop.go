abfa3b8e97be418ab142d85e4927b78b526f2b86
Debdut <debdutdeb@outlook.com>
2026-09-03 11:33:30 +0530

    feat: add `ReadNer` implementation

--- stdutils/reader/noop.go
+++ stdutils/reader/noop.go
@@ -6,16 +6,17 @@ type noopReader struct {
 	io.Reader
 }
 
-var (
-	_ io.Reader  = &noopReader{}
-	_ ReadHasher = &noopReader{}
-)
-
 // NopHasher returns a ReadHasher with No-op Sum implementation.
 func NopHasher(reader io.Reader) ReadHasher {
+	return &noopReader{Reader: reader}
+}
+
+func NopNer(reader io.Reader) ReadNer {
 	return &noopReader{Reader: reader}
 }
 
 func (r *noopReader) Sum([]byte) []byte {
 	return nil
 }
+
+func (r *noopReader) ByteCount() int { return -1 }
---
+++ stdutils/reader/size.go
@@ -0,0 +1,31 @@
+package reader
+
+import "io"
+
+// ReadNer is a super-interface that returns the total byte count of what was read from it.
+//
+// NOTE(self): This is a bit weirder name. But I'm gonna keep it. Inspiration is from `n, err := r.Read()`, `n` is the convention, so `Read`+`N`+`er`.
+type ReadNer interface {
+	io.Reader
+	ByteCount() int
+}
+
+type readNer struct {
+	io.Reader
+	n int
+}
+
+// NewNer returns a ReadNer interface from an [io.Reader]
+func NewNer(reader io.Reader) ReadNer {
+	return &readNer{Reader: reader, n: 0}
+}
+
+func (r *readNer) Read(buffer []byte) (n int, err error) {
+	n, err = r.Reader.Read(buffer)
+	r.n += n
+	return n, err
+}
+
+func (r *readNer) ByteCount() int {
+	return r.n
+}
